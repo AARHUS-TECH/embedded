2cc90eb2e6488c2d726880a4db7e2ab19a8dae6b
Emil766b <emil766b@edu.aarhustech.dk>
2021-06-21 09:04:14 +0200

    Relocated database connection string

--- Webinterface/backend/src/app.module.ts
+++ Webinterface/backend/src/app.module.ts
@@ -5,6 +5,13 @@ import { AuthController } from './controllers/auth/auth.controller';
 import { DrivhusController } from './controllers/drivhus/drivhus.controller';
 import { HoensehusController } from './controllers/hoensehus/hoensehus.controller';
 import { DashboardController } from './controllers/dashboard/dashboard.controller';
+import { Injectable } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb+srv://User:Datait2021@Database.hsfog.mongodb.net/Cluser0?retryWrites=true&w=majority')],
+})
+
 
 @Module({
   imports: [],
--- Webinterface/backend/src/app.service.ts
+++ Webinterface/backend/src/app.service.ts
@@ -1,14 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-
-@Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://User:Datait2021@Database.hsfog.mongodb.net/Cluser0?retryWrites=true&w=majority')],
-})
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello world!';
   }
 }
