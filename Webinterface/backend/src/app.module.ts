e485d09ac67fb1afbc336e5f53ca0d8d4ee9e677
niko7381 <42538560+niko7381@users.noreply.github.com>
2021-06-22 13:27:55 +0200

    appmodule

--- Webinterface/backend/src/app.module.ts
+++ Webinterface/backend/src/app.module.ts
@@ -1,23 +1,40 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthController } from './controllers/auth/auth.controller';
-import { DrivhusController } from './controllers/drivhus/drivhus.controller';
-import { HoensehusController } from './controllers/hoensehus/hoensehus.controller';
-import { DashboardController } from './controllers/dashboard/dashboard.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { AppController } from './app.controller';
+import { AuthController } from './controllers/auth/auth.controller';
+import { DrivhusController } from './controllers/drivhus/drivhus.controller';
+import { UserController } from './controllers/User/user.controller';
+import { DashboardController } from './controllers/dashboard/dashboard.controller';
+
+import { AppService } from './app.service';
+import { AuthService } from './services/auth/auth.service'
+import { DashboardService } from './services/dashboard/dashboard.service';
+import { DrivhusService } from './services/drivhus/drivhus.service';
+import { UserService } from './services/user/user.service';
+
+const MongoClient = require('mongodb').MongoClient;
+
+import { UserSchema } from './models/user.model';
+
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://User:Datait2021@Database.hsfog.mongodb.net/Cluser0?retryWrites=true&w=majority')],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://ProjektStyring:SKPDataIT2021@cluster0.hsfog.mongodb.net/Cluster0?retryWrites=true&w=majority'),
+    MongooseModule.forFeature([{ name: 'UserSchema', schema: UserSchema }])
+  ],
   controllers: [
     AppController, 
     AuthController,
     DrivhusController,
-    HoensehusController,
+    UserController,
     DashboardController
   ],
   providers: [
-    AppService
+    AppService,
+    AuthService,
+    DashboardService,
+    DrivhusService,
+    UserService
   ],
 })
 export class AppModule {}
